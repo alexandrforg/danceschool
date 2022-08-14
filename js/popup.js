fe8f81438c9627ef65f47d5f284213af4f972f98
alexandrforg <alexandr.forg17@gmail.com>
2022-08-14 22:46:05 +0300

    f1408

---
+++ js/popup.js
@@ -0,0 +1,48 @@
+
+let btns = document.querySelectorAll("[data-modal-btn]"); 
+    
+const modalOverlay = document.querySelector('.modal-overlay ');
+
+
+
+  btns.forEach((el) => {
+   el.addEventListener('click', (e) => {
+     let path = e.currentTarget.getAttribute('data-modal-btn');
+     const modals = document.querySelectorAll("[data-modal-window]");
+    
+  
+    modals.forEach((el) => {
+        el.classList.remove("modal--visible"); 
+        modalOverlay.classList.remove("modal-overlay--visible");
+    });
+
+    document.querySelector(`[data-modal-window = "${path}"]`).classList.add("modal--visible");
+    modalOverlay.classList.add('modal-overlay--visible');
+     
+     
+   
+
+
+    if(path ==="home"){
+     modalOverlay.classList.remove("modal-overlay--visible");
+    }
+
+  });
+
+  
+     
+   
+
+});
+
+  modalOverlay.addEventListener('click', (e) => {
+  let path = e.currentTarget.getAttribute('data-modal-btn');
+
+
+  if (e.target == modalOverlay  ) {
+     modalOverlay.classList.remove('modal-overlay--visible');
+     modals.forEach((el) => {
+       el.classList.remove('modal--visible');
+    });
+   }
+});
